2ab43abc940b073b1db7e8a11fd0cce6c51b505b
BravoTwoZero <sofiul.alam@hotmail.com>
2020-11-23 22:25:49 +0000

    Added Milk

--- src/entities/dough.entity.ts
+++ src/entities/dough.entity.ts
@@ -3,20 +3,24 @@ import { Dough } from "../interface/IDough";
 import { TYPES } from "../types/types";
 import { Flour } from "../interface/IFlour";
 import { Yeast } from "../interface/IYeast";
+import { Milk } from "../interface/IMilk";
 
 @injectable()
 export class DoughEntity implements Dough {
 
     public flour: Flour;
     public yeast: Yeast;
+    public milk: Milk;
 
     public constructor(
         @inject(TYPES.Flour) flour: Flour,
         @inject(TYPES.Yeast) yeast: Yeast,
+        @inject(TYPES.Milk) milk: Milk,
     ) {
         console.log('Dough class initiated');
         this.flour = flour;
         this.yeast = yeast;
+        this.milk = milk;
     }
 
     public getFlour(): Flour {
@@ -24,5 +28,8 @@ export class DoughEntity implements Dough {
     }
     public getYeast(): Yeast {
         return this.yeast;
+    }
+    public getMilk(): Milk {
+        return this.milk;
     }
 }
